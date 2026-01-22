b4db154fa4727509e2515422aab466276204fc3b
TaeGaram (Office) <drasla217@gmail.com>
2026-01-22 17:41:24 +0900

    API 문서 중 프로젝트 이름 및 설명 업데이트

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,12 +1,10 @@
-import { Router } from 'express';
-import { AuthController } from '../controllers/auth.controller';
-import { authenticateJwt } from "../middlewares/auth.middleware";
+import { Router } from "express";
+import { AuthController } from "../controllers/auth.controller";
 
 const authRoute = Router();
 const authController = new AuthController();
 
-authRoute.use(authenticateJwt);
-authRoute.post('/register', authController.register);
-authRoute.post('/login', authController.login);
+authRoute.post("/register", authController.register);
+authRoute.post("/login", authController.login);
 
 export default authRoute;
